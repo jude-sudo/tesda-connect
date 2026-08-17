ae4c29f3c1769e16a2eb013c82883e6be49ad113
jude-sudo <60461710+jude-sudo@users.noreply.github.com>
2026-08-17 09:25:41 +0800

    Refactor middleware setup to trust proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,12 +12,14 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->alias([
-            'permission' => \App\Http\Middleware\CheckPermission::class,
-            'role' => RoleMiddleware::class,        ]);
-        
-    })
+->withMiddleware(function (Middleware $middleware): void {
+    $middleware->trustProxies(at: '*');
+
+    $middleware->alias([
+        'permission' => \App\Http\Middleware\CheckPermission::class,
+        'role' => RoleMiddleware::class,
+    ]);
+})
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
